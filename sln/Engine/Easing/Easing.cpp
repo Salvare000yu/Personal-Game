291e6e5a54adf3381ed28c18246f02333ef83a4c
Salvare000 <k020g1393@g.neec.ac.jp>
2023-07-23 22:23:30 +0900

    EaseInElastic実装

--- sln/Engine/Easing/Easing.cpp
+++ sln/Engine/Easing/Easing.cpp
@@ -15,18 +15,38 @@ float Easing::EaseIn(const float t, const float startPos, const float endPos, co
 	return currentPos;
 }
 
+float Easing::EaseInElastic(const float t, const float startPos, const float endPos, const float totalTime)
+{
+	// 間隔
+	const float distTime = t / totalTime;
+
+	const float t2 = std::pow(distTime, 2.f);
+
+	// 計算結果一時格納
+	const float result = t2 * t2 * std::sin(distTime * XM_PI * 4.5f);
+
+	// 場所の間隔
+	const float distPos = endPos - startPos;
+	// 現在位置
+	const float currentPos = startPos+distPos * result;
+
+	return currentPos;
+}
+
 float Easing::EaseOutElastic(const float t, const float startPos, const float endPos, const float totalTime)
 {
 	// 間隔
 	const float distTime = t / totalTime;
 
 	const float t2 = std::pow(distTime-1, 2.f);
+
 	// 計算結果一時格納
 	const float result = 1 - t2 * t2 * std::cos(distTime * XM_PI * 4.5f);
 
 	// 場所の間隔
 	const float distPos = endPos - startPos;
-	const float currentPos= distPos + distPos * -result+ startPos;
+	// 現在位置 -result
+	const float currentPos= startPos+distPos + distPos * -result;
 
 	return currentPos;
 }
--- sln/Engine/Easing/Easing.h
+++ sln/Engine/Easing/Easing.h
@@ -25,7 +25,21 @@ public:
 		const float totalTime
 	);
 
-	static float EaseOutElastic(const float t,
+	/// @brief 揺れてから速くなっていく
+	/// @param t 時間
+	/// @param startPos 開始位置
+	/// @param endPos 最大位置
+	/// @param totalTime 移動にかける時間
+	/// @return 現在位置
+	static float EaseInElastic(
+		const float t,
+		const float startPos,
+		const float endPos,
+		const float totalTime
+	);
+
+	static float EaseOutElastic(
+const float t,
 		const float startPos,
 		const float endPos,
 		const float totalTime
