66ec1968843f01dd5f60e17db83006f751e765a5
Salvare000 <k020g1393@g.neec.ac.jp>
2023-07-24 15:00:43 +0900

    EaseInQuad実装

--- sln/Engine/Easing/Easing.cpp
+++ sln/Engine/Easing/Easing.cpp
@@ -69,6 +69,28 @@ float Easing::EaseInOutSine(
 	return currentPos;
 }
 
+float Easing::EaseInQuad(
+	const float t,
+	const float startPos,
+	const float endPos, 
+	const float totalTime
+)
+{
+	// 間隔
+	const float distTime = t / totalTime;
+
+	// 場所の間隔
+	const float distPos = endPos - startPos;
+
+	// 計算結果一時格納
+	const float result = std::pow(distTime,2.f);
+
+	// 現在位置
+	const float currentPos = distPos * result + startPos;
+
+	return currentPos;
+}
+
 float Easing::EaseOutQuad(
 	const float t,
 	const float startPos,
--- sln/Engine/Easing/Easing.h
+++ sln/Engine/Easing/Easing.h
@@ -51,6 +51,19 @@ public:
 		const float totalTime
 	);
 
+	/// @brief イーズイン(Quad)
+	/// @param t 時間
+	/// @param startPos 開始位置
+	/// @param endPos 最大位置
+	/// @param totalTime 移動にかける時間
+	/// @return 現在の位置
+	static float EaseInQuad(
+		const float t,
+		const float startPos,
+		const float endPos,
+		const float totalTime
+	);
+
 	/// @brief イーズアウト(Quad)
 	/// @param t 時間
 	/// @param startPos 開始位置
